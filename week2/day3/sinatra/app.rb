df6f055eb69d0831c39ec77c5455cb12155943f9
Javier Echevarria <javierechevarria21@yahoo.com>
2016-08-24 16:10:26 -0400

    add sinatra part 1 exercise

---
+++ week2/day3/sinatra/app.rb
@@ -0,0 +1,64 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry"
+
+get "/" do
+	"hello <a href='/company'> See company page </a>"
+end
+
+get "/company" do
+	"We are looking for investments,  <a href='/company/about'> please visit our about page. </a>"
+end
+
+
+get "/company/about" do
+  
+  erb :about 
+end
+
+get "/company/pizza" do
+   @pizza_ingredients = ["Cheese", "Tomatoe Sauce", "Sausage"]
+  erb :favorite_pizza
+end	
+
+
+users = [
+  { :username => "starwars",     :name => "Star Wars",     :bio => "The official home of Star Wars on Twitter.",           :avatar => "https://pbs.twimg.com/profile_images/631535425333518336/D-i_GqpT_400x400.jpg" },
+  { :username => "celinedion",   :name => "Celine Dion",   :bio => "Posts signed / Publications signées TC = Team Céline", :avatar => "https://pbs.twimg.com/profile_images/733642354565996544/JrzvO3Y7_400x400.jpg" },
+  { :username => "khalifenizar", :name => "Nizar Khalife", :bio => "I like pizza.",                                        :avatar => "https://pbs.twimg.com/profile_images/480477171362701313/HfqN0EU7_400x400.jpeg" }
+]
+
+
+# http://localhost:4567/users/ironhack
+# http://localhost:4567/users/starwars
+#                             --------
+#                                |
+#               ------------------
+#               |
+get "/users/:username" do
+    #           |
+    #           ------------------
+    #                            |
+  @user_name_string = params[:username]
+
+  @the_user = users.find { |the_user| the_user[:username] == @user_name_string }
+
+  # binding.pry
+
+  if @the_user == nil
+    erb :no_user
+  else
+    erb :user_profile
+  end
+end
+
+enable(:sessions)
+
+get"/session_test/:text" do
+  text = params[:text]
+  session[:saved_value] = text
+end
+
+get "/session_show" do
+  "Now in the session: " + session[:saved_value]
+end
