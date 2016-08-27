c456e0a060015a4881f4ca9fc5f0be07ee759ea5
Javier Echevarria <javierechevarria21@yahoo.com>
2016-08-27 11:21:39 -0400

    add movie trivia code

---
+++ week2/day5/moviegame/app.rb
@@ -0,0 +1,28 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "imdb"
+
+
+
+get"/" do
+
+erb :home
+
+end
+
+post "/search_results" do
+
+	the_search = Imdb::Search.new( params[:movie_search])
+
+	twenty_movies = the_search.movies.take(20)
+    twenty_movies.delete_if { |the_movie| the_movie.poster == nil}
+    @movies = twenty_movies.take(9)
+
+	erb :results
+	
+end
+
+
+
+
+
