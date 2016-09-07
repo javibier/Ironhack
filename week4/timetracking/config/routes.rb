c96d5fc08fe0d5317f118cf2df27445c278376ae
Javier Echevarria <javierechevarria21@yahoo.com>
2016-09-07 15:26:58 -0400

    add seeds update to timetracking

--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	get '/', to: 'site#home'
 	get '/contact' , to: 'site#email'
 	get '/say_name/:name' , to: 'site#say_name'
+	get '/projects', to: 'projects#index'
 
 end
 
--- week4/timetracking/db/seeds.rb
+++ week4/timetracking/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.create(name: 'Ironhack', description: 'Coding Bootcamp')
+
+Project.create(name: 'Time tracking app')
+
+Project.create(name: 'Recipes', description: 'Track my favorite recipes')
+
+Project.create(name: 'Hemo', description: 'Track my favorite uses of Hemp')
+
+Project.create(name: 'Home', description: 'Where the heart is')
+
+Project.create(name: 'Cars', description: 'I would love to restore old cars')
+
+Project.create(name: 'Seltzer Water', description: 'The benefits of drinking seltzer water')
+
+Project.create(name: 'Travel', description: 'Track my favorite places to travel')
+
+Project.create(name: 'Dinner', description: 'Track my favorite restaurants')
+
+Project.create(name: 'Movies', description: 'Track my favorite movies')
