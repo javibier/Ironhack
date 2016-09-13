a3c8e6cc6eee429a442857d2d7a76f55e83f2e18
Javier Echevarria <javierechevarria21@yahoo.com>
2016-09-13 15:58:24 -0400

    add pizza exercise

---
+++ week5/day1/jquery/app.js
@@ -0,0 +1,27 @@
+  $(document).ready (function () {
+
+
+//   $('.hello-button').on('click', function () {
+//         alert('hello world');
+//       });
+
+//   $('.js-taco').on('click', function () {
+//         alert('I am hungry, I would love a Taco!!');
+
+//         $(".container").append("<h1> Tacos 1</h1>")
+//         $(".container").prepend("<h1> Tacos 2</h1>")
+//         $(".container").before("<h1> Tacos 3</h1>")
+//         $(".container").after("<h1> Tacos 4</h1>")
+//       });
+
+// });
+
+  $('input').prop('type');
+  $('.container').prop('class')
+
+  $('#hideshow').on('click', function (event) {
+        $('.container').toggle('show')
+
+      });
+
+    });
