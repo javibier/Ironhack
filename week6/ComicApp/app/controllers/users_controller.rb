c71533a58ce57c885cda4cae7306ba7e6e64b61c
Javier Echevarria <javierechevarria21@yahoo.com>
2016-09-21 15:18:26 -0400

    add devise exercise

---
+++ week6/ComicApp/app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+	before_action :authenticate_user!, only: [:show]
+
+	def home
+
+        @users = User.all
+		render :home
+
+	end
+
+	def show
+
+		@user = User.find(params[:id])
+		render :show
+
+	end
+
+end
