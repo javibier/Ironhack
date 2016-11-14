4f3ea5e1824e2b04f076921d5d67451398128d8c
Javier Echevarria <javierechevarria21@yahoo.com>
2016-11-14 10:04:32 -0500

    update landing page on personal site app

--- PersonalSite/config/routes.rb
+++ PersonalSite/config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'home/name:string'
-
-  get 'home/image:string'
-
-  get 'home/description:text'
-
   root "sites#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
