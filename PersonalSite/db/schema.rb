78dcb90a0235ce22748784fd456ff298d360f530
Javier Echevarria <javierechevarria21@yahoo.com>
2016-11-14 09:58:04 -0500

    update landing page on personal site app

--- PersonalSite/db/schema.rb
+++ PersonalSite/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111144305) do
+ActiveRecord::Schema.define(version: 20161114145619) do
+
+  create_table "homes", force: :cascade do |t|
+    t.string   "name"
+    t.string   "image"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "sites", force: :cascade do |t|
     t.string   "name"
