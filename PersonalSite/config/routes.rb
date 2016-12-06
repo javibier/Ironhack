1c491dd9eafd4488e683f309aa9f4571595b1f83
Javier Echevarria <javierechevarria21@yahoo.com>
2016-12-06 10:20:57 -0500

    add projects on personal site app

--- PersonalSite/config/routes.rb
+++ PersonalSite/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   get '/abouts', to: 'abouts#show'
 
+  get '/projects', to: 'projects#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
