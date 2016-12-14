1a5540b97c957fc9893b842b13e7a3973aa579fe
Javier Echevarria <javierechevarria21@yahoo.com>
2016-12-14 17:09:34 -0500

    add contact page on personal site app

--- PersonalSite/config/routes.rb
+++ PersonalSite/config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   get '/projects', to: 'projects#home'
 
-  get '/contacts', to: 'contactss#home'
+  get '/contacts', to: 'contacts#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
